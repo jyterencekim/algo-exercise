33fbd6a1da1c84334fa65de3be1b1a1a03da30fa
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2022-07-31 22:22:23 +0900

    Time: 307 ms (22.66%), Space: 13.9 MB (93.51%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,30 +1,21 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        S = len(s)
-        max_len = 1
+        max_len = 0
+        freq = Counter()
+        most_freq = 0
+        start = 0
         
-        left, right = 0, 0
-        counter = Counter()
-        while right < S:
-            sublength = right - left + 1
-            counter[s[right]] += 1
-            chars = list((+counter).keys())
-            if len(chars) == 1:
-                max_len = max(max_len, sublength)
-            else: 
-                majority = max(chars, key=lambda k:counter[k])
-                majority_count = counter[majority]
-                minority_count = sublength - majority_count
-                if minority_count <= k:
-                    # all the minorities can be converted to the majority char
-                    max_len = max(max_len, counter[majority] + minority_count)
-                else:
-                    counter[s[left]] -= 1
-                    counter[s[right]] -= 1
-                    left += 1
-                    continue
-            right += 1
+        for end, c in enumerate(s):
+            freq[c] += 1
+            most_freq = max(most_freq, freq[c])
+            
+            while (end - start + 1) - most_freq > k:
+                freq[s[start]] -= 1
+                start += 1
+            
+            max_len = max(max_len, (end - start + 1))
         
         return max_len
+                
                                 
                     
