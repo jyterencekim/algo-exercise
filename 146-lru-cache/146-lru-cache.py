d1020f609292eb658f1233feba926f237d0132cd
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2022-08-03 01:29:00 +0900

    Time: 1672 ms (21.75%), Space: 76.7 MB (11.03%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -1,97 +1,73 @@
-class DLinkedNode(): 
-    def __init__(self):
-        self.key = 0
-        self.value = 0
-        self.prev = None
-        self.next = None
-            
-class LRUCache():
-    def _add_node(self, node):
-        """
-        Always add the new node right after head.
-        """
-        node.prev = self.head
-        node.next = self.head.next
-
-        self.head.next.prev = node
-        self.head.next = node
-
-    def _remove_node(self, node):
-        """
-        Remove an existing node from the linked list.
-        """
-        prev = node.prev
-        new = node.next
-
-        prev.next = new
-        new.prev = prev
-
-    def _move_to_head(self, node):
-        """
-        Move certain node in between to the head.
-        """
-        self._remove_node(node)
-        self._add_node(node)
-
-    def _pop_tail(self):
-        """
-        Pop the current tail.
-        """
-        res = self.tail.prev
-        self._remove_node(res)
-        return res
-
-    def __init__(self, capacity):
-        """
-        :type capacity: int
-        """
-        self.cache = {}
-        self.size = 0
-        self.capacity = capacity
-        self.head, self.tail = DLinkedNode(), DLinkedNode()
-
-        self.head.next = self.tail
-        self.tail.prev = self.head
+class Node:
+    
+    def __init__(self, key: int=None, value: int=None, pre: 'Node'=None, nxt: 'None'=None):
+        self.key = key
+        self.value = value
+        self.pre = pre
+        self.nxt = nxt
         
+        
+        
+class LRUCache:
+    
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.nodes = dict()
+        self.mru = Node()
+        self.lru = Node() # linked-list head/tail pointers
+        self.mru.nxt = self.lru
+        self.lru.pre = self.mru
+        self.size = 0
 
-    def get(self, key):
-        """
-        :type key: int
-        :rtype: int
-        """
-        node = self.cache.get(key, None)
-        if not node:
+    def remove(self, node: Node) -> None:
+        pre = node.pre
+        nxt = node.nxt
+        pre.nxt = nxt
+        nxt.pre = pre
+        
+    def push_head(self, node: Node) -> None:
+        node.pre = self.mru
+        node.nxt = self.mru.nxt
+        
+        self.mru.nxt.pre = node
+        self.mru.nxt = node
+    
+    def pop_tail(self) -> Node:
+        old_tail = self.lru.pre
+        old_tail.pre.nxt = self.lru
+        self.lru.pre = old_tail.pre
+        return old_tail
+        
+    def refresh(self, node: Node) -> None:
+        self.remove(node)
+        self.push_head(node)
+        
+    def get(self, key: int) -> int:
+        if key not in self.nodes:
             return -1
+        curr = self.nodes[key]
+        self.refresh(curr)
+        return curr.value
 
-        # move the accessed node to the head;
-        self._move_to_head(node)
-
-        return node.value
-
-    def put(self, key, value):
-        """
-        :type key: int
-        :type value: int
-        :rtype: void
-        """
-        node = self.cache.get(key)
-
-        if not node: 
-            newNode = DLinkedNode()
-            newNode.key = key
-            newNode.value = value
-
-            self.cache[key] = newNode
-            self._add_node(newNode)
-
-            self.size += 1
-
+    def put(self, key: int, value: int) -> None:
+        node = self.nodes.get(key)
+        
+        if not node:
+            new_node = Node(key, value)
+            self.push_head(new_node)
+            self.nodes[key] = new_node
+            self.size += 1    
+            
             if self.size > self.capacity:
-                # pop the tail
-                tail = self._pop_tail()
-                del self.cache[tail.key]
+                tail = self.pop_tail()
+                del self.nodes[tail.key]
                 self.size -= 1
         else:
-            # update the value.
             node.value = value
-            self._move_to_head(node)
+            self.refresh(node)
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
