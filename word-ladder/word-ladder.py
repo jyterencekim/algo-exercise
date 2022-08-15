99c4405270a273ccbb60da970e8457fdc3febeab
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2022-08-15 19:27:35 +0900

    Time: 2166 ms, Memory: 22.5 MB - LeetHub

---
+++ word-ladder/word-ladder.py
@@ -0,0 +1,45 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        graph = defaultdict(set)
+        relatives = defaultdict(set)
+        ANY = '*'
+        
+        def to_relatives(x: str) -> List[str]:
+            return [x[:i] + ANY + x[i + 1:] for i in range(len(x))]
+        
+        def are_adjacents(a: str, b: str) -> bool:
+            return len([c for i, c in enumerate(a) if c != b[i]]) == 1
+        
+        def get_adjacents(x: str) -> List[str]:
+            nonlocal relatives
+            result = []
+            for relative in to_relatives(x):
+                result.extend(relatives[relative])
+            return result
+                
+        
+        words = set(wordList)
+        words.add(beginWord)
+        
+        if endWord not in words:
+            return 0
+        
+        for a in words:
+            for rel in to_relatives(a):
+                relatives[rel].add(a)
+        
+        q = deque([(beginWord, 1)])
+        visited = set()
+        
+        while q:
+            word, dist = q.popleft()
+            visited.add(word)
+            
+            if word == endWord:
+                return dist
+            
+            for adjacent in get_adjacents(word):
+                if adjacent not in visited:
+                    q.append((adjacent, dist + 1))
+        
+        return 0
