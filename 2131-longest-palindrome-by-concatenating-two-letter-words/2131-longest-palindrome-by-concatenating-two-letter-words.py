1b1dd45a5c31b49cdf2f41aa57ab2b35e700ad0b
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2022-09-09 21:03:55 +0900

    Time: 3259 ms (5.37%), Space: 38.6 MB (7.10%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -3,17 +3,20 @@ class Solution:
         left = Counter(words)
         specials = { word for word in words if word[0] == word[1] }
         longest = 0
+        words_set = set(words)
         
-        for word in words:
+        for word in words_set:
             rev = word[::-1]
             if word in specials:
                 if left[word] >= 2:
-                    longest += 4
-                    left[word] -= 2
+                    pairable = left[word] // 2
+                    longest += pairable * 4 
+                    left[word] -= pairable * 2
             elif left[word] and left[rev]:
-                longest += 4
-                left[word] -= 1
-                left[rev] -= 1
+                usable = min(left[word], left[rev])
+                longest += usable * 4
+                left[word] -= usable
+                left[rev] -= usable
         
         for word in specials:
             if left[word]:
