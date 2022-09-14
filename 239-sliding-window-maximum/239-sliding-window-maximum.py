07bbf24877f3f107b918fff774f666fc56194c88
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2022-09-14 21:16:51 +0900

    Time: 4586 ms (7.71%), Space: 27.7 MB (99.73%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -1,39 +1,26 @@
 class Solution:
-    def maxSlidingWindow(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        class MaxWindowStream:
-          def __init__(self, window_size=3):
-            self.window_size = window_size
-            self.window_heap = [(float('inf'), 0)] 
-            self.current_index = 0
-
-
-          def input(self, value):
-            # Determine the inclusive lower bound for the valid index
-            # in order to delete old data from the heap 
-            valid_index_lower_bound = self.current_index - self.window_size + 1
-
-            # Keep popping until we find the max with a valid index
-            while self.window_heap and self.window_heap[0][1] < valid_index_lower_bound:
-              heapq.heappop(self.window_heap)
-
-            new_node = (-value, self.current_index)
-            self.current_index += 1
-            heapq.heappush(self.window_heap, new_node)
-
-            max_within_window = -self.window_heap[0][0]
-            return max_within_window
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        if not nums:
+            return []
+        if k == 1:
+            return nums
         
-        result = list()
-        stream = MaxWindowStream(k)
-        for i in range(k - 1):
-            stream.input(nums[i])
+        result = []
+        q = deque() # (value, index)
         
-        for num in nums[k - 1:]:
-            result.append(stream.input(num))
+        for i, num in enumerate(nums):
+            # maintenance
+            while q and q[0][1] <= i - k:
+                q.popleft()
+
+            while q and q[-1][0] < num:
+                q.pop()
+
+            if not q or q[-1][0] >= num:
+                q.append((num, i))
             
-        return result
+            result.append(q[0][0])
+        
+        return result[k - 1:]
+            
+        
