b2aa34c5796cf2d6079048ba4bf9e7210ea40bdc
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2022-09-18 21:38:51 +0900

    Time: 87 ms (50.58%), Space: 19.1 MB (10.91%) - LeetHub

---
+++ 110-balanced-binary-tree/110-balanced-binary-tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def probe(node: Optional[TreeNode], depth: int) -> int:
+            if not node:
+                return depth
+            l = probe(node.left, depth + 1)
+            r = probe(node.right, depth + 1)
+            if not l or not r:
+                return None
+            if abs(l - r) > 1:
+                return None
+            return max(l, r)
+        
+        depth = probe(root, 0)
+        return depth is not None
+            
