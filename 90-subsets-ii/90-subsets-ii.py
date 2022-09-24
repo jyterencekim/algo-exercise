00ae5856525b7f69c44248f2cf5365d2d57cd08e
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2022-09-24 15:32:16 +0900

    Time: 53 ms (70.41%), Space: 14.2 MB (10.34%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -1,15 +1,13 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        answer = set()
-        def subset(ptr: int, carry: Tuple[int]) -> None:
-            if ptr >= len(nums):
-                nonlocal answer
-                answer.add(carry)
-                return
-            subset(ptr + 1, carry)
-            subset(ptr + 1, carry + (nums[ptr],))
+        subsets = {()}
         
-        subset(0, tuple())
-        return answer
+        for num in nums:
+            generated = set(subsets)
+            for s in subsets:
+                generated.add(s + (num,))
+            subsets = generated
+            
+        return subsets
             
