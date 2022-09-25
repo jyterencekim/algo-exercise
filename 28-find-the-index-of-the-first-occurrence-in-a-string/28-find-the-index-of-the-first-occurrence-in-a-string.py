43162e3c8ffee73847b01f26468a1a297683f6bd
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2022-09-25 15:28:35 +0900

    Time: 50 ms (50.03%), Space: 13.8 MB (65.09%) - LeetHub

--- 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -3,18 +3,40 @@ class Solution:
         if not haystack or len(needle) > len(haystack):
             return -1
         
+        H, N = len(haystack), len(needle)
+        lps = [0 for _ in range(N)]
+        
+        # precompute lps
+        max_len = 0
+        i = 1
+        
+        while i < N:
+            if needle[max_len] == needle[i]:
+                max_len += 1
+                lps[i] = max_len
+                i += 1
+            else:
+                if max_len:
+                    max_len = lps[max_len - 1]
+                else:
+                    lps[i] = 0
+                    i += 1
+        
+        
         reader, comparer = 0, 0
         
-        while reader < len(haystack):
+        while reader < H:
             if haystack[reader] == needle[comparer]:
                 comparer += 1
+                reader += 1
             else:
-                reader -= comparer
-                comparer = 0
-            
+                if comparer:
+                    comparer = lps[comparer - 1]
+                else:
+                    reader += 1
+                
             if comparer == len(needle):
-                return reader - comparer + 1
+                return reader - comparer
             
-            reader += 1
         
         return -1
