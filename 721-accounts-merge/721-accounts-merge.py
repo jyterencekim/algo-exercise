04915bd4f41e796248a6525027c30c4cf84df5da
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2022-09-26 21:57:16 +0900

    Time: 528 ms (25.29%), Space: 18.1 MB (71.32%) - LeetHub

---
+++ 721-accounts-merge/721-accounts-merge.py
@@ -0,0 +1,40 @@
+import heapq
+from collections import deque, defaultdict
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        roots = dict()
+        
+        def find_root(email: str) -> str:
+            if email not in roots:
+                roots[email] = email
+            if roots[email] != email:
+                roots[email] = find_root(roots[email])
+            return roots[email]
+        
+        def connect(x: str, y: str) -> str:
+            xr, yr = find_root(x), find_root(y)
+            roots[xr] = yr
+            return xr
+        
+        def are_connected(x: str, y: str) -> bool:
+            return find_root(x) == find_root(y)
+        
+        names_by_email = dict()
+        
+        for account in accounts:
+            name, emails = account[0], account[1:]
+            for email in emails:
+                connect(emails[0], email)
+                names_by_email[email] = name
+        
+        mails_by_root = defaultdict(list)
+        for k, v in roots.items():
+            mails_by_root[find_root(v)].append(k)
+        
+        for root, mails in mails_by_root.items():
+            name = names_by_email[root]
+            yield [name] + sorted(mails)
+        
+        
+            
