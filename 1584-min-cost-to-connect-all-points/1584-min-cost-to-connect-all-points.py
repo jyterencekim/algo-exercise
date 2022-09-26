774d6423aa6b948eff7f1f5c0c0991682cc2394e
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2022-09-26 22:26:48 +0900

    Time: 3283 ms (58.34%), Space: 14.4 MB (96.23%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,40 +1,25 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        def distance(x: Tuple[int], y: Tuple[int]) -> int:
-            return abs(x[0] - y[0]) + abs(x[1] - y[1])
+        N = len(points)
+        min_dist = [math.inf for _ in range(N)]
         
-        # Kruskal
-        roots = dict()
-        for x, y in points:
-            roots[(x, y)] = (x, y)
-            
-        def find_root(p: Tuple[int]) -> Tuple[int]:
-            if roots[p] != p:
-                roots[p] = find_root(roots[p])
-            return roots[p]
+        # starting point
+        mst = { tuple(points[0]) }
+        min_dist[0] = 0
         
-        def connect(p1: Tuple[int], p2: Tuple[int]) -> Tuple[int]:
-            roots[find_root(p1)] = find_root(p2)
-            return find_root(p1)
-            
-        def are_connected(p1, p2) -> bool:
-            return find_root(p1) == find_root(p2)
+        def get_distance(a, b) -> int:
+            return abs(a[0] - b[0]) + abs(a[1] - b[1])
         
-        edges = [] # heap
-        for i, p1 in enumerate(points):
-            for j in range(i + 1, len(points)):
-                p2 = points[j]
-                heapq.heappush(edges, (distance(p1, p2), tuple(p1), tuple(p2)))
+        frontier = tuple(points[0])
+        while len(mst) < N:
+            optimal_dist, closest = math.inf, None
+            for j, y in enumerate(points):
+                if tuple(y) not in mst:
+                    min_dist[j] = min(min_dist[j], get_distance(frontier, y))
+                    if min_dist[j] < optimal_dist:
+                        optimal_dist, closest = min_dist[j], y
+                
+            mst.add(tuple(closest))
+            frontier = tuple(closest)
         
-        to_connect = len(points)
-        cost_acc = 0
-        
-        while to_connect > 1:
-            d, p1, p2 = heapq.heappop(edges)
-            if not are_connected(p1, p2):
-                connect(p1, p2)
-                cost_acc += d
-                to_connect -= 1
-        
-        return cost_acc
-            
+        return sum(min_dist)
