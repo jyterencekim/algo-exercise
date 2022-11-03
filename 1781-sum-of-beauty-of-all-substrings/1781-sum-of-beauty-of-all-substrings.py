cead0091f80d46c6d0df625a71eb4e813d547bb1
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2022-11-03 21:15:45 +0900

    Time: 3704 ms (80.63%), Space: 13.8 MB (97.63%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
@@ -1,33 +1,18 @@
 class Solution:
     def beautySum(self, s: str) -> int:
-        def calculate_beauty(counts: dict) -> int:
-            min_c, max_c = None, None
-            min_freq, max_freq = math.inf, -math.inf
-            
-            for c, freq in counts.items():
-                if freq == 0:
-                    continue
-                if freq > max_freq:
-                    max_c, max_freq = c, freq
-                if freq < min_freq:
-                    min_c, min_freq = c, freq
-            
-            if min_c != max_c:
-                return max_freq - min_freq
-            return 0
-            return max(counts.values()) - min(counts.values())
+        def beautify(freqs: List[int]) -> int:
+            return max(freqs) - min(f for f in freqs if f)
+        def to_ord(c: chr) -> int:
+            return ord(c) - ord('a')
         
         beauty_sum = 0
-        
-        for offset in range(1, len(s)):
-            counts = Counter(s[:offset+1])
-            beauty_sum += calculate_beauty(counts)
-            
-            for left in range(1, len(s) - offset):
-                right = left + offset
-                counts[s[left - 1]] -= 1
-                counts[s[right]] += 1
-                beauty_sum += calculate_beauty(counts)
+        S = len(s)
+        for left in range(S - 1):
+            freqs = [0] * 26
+            for right in range(left, S):
+                freqs[to_ord(s[right])] += 1
+                beauty_sum += beautify(freqs)
+                
         
         return beauty_sum
                 
