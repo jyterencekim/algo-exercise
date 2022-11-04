2d975d976e35129182ad724bfed07afc4db5a812
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2022-11-04 18:31:25 +0900

    Time: 2107 ms (48.46%), Space: 15.5 MB (70.67%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,37 +1,30 @@
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        # does it help to be greedy? may not be so at the end.
-        # let's just consider some paths ... let's say we have two paths, of effort E1 and effort E2.
-        # if those paths can be connected, then the combined path's effort will be max(E1, E2) 
         ROWS, COLS = len(heights), len(heights[0])
+        min_efforts = [[math.inf for _ in range(COLS)] for _ in range(ROWS)]
+        min_efforts[0][0] = 0
         
-        # generator
         def get_adjacents(r: int, c: int) -> Tuple[int]:
             if r > 0:
-                yield (r - 1, c)
+                yield r - 1, c
             if c > 0:
-                yield (r, c - 1)
+                yield r, c - 1
             if r < ROWS - 1:
-                yield (r + 1, c)
+                yield r + 1, c
             if c < COLS - 1:
-                yield (r, c + 1)
+                yield r, c + 1
         
-        # priority queue of (effort, r, c)
-        q_by_effort = [(0, 0, 0)] 
-        min_efforts = defaultdict(lambda: math.inf)
+        pq = [(0, (0, 0))]
         
-        while q_by_effort:
-            effort_so_far, r, c = heapq.heappop(q_by_effort)
-            
+        while pq:
+            effort, pos = heapq.heappop(pq)
+            r, c = pos
             if (r, c) == (ROWS - 1, COLS - 1):
-                return effort_so_far
-            
-            for ar, ac in get_adjacents(r, c):
-                effort_to_adjacent = max(abs(heights[r][c] - heights[ar][ac]), effort_so_far)
-                if effort_to_adjacent < min_efforts[(ar, ac)]:
-                    min_efforts[(ar, ac)] = effort_to_adjacent
-                    heapq.heappush(q_by_effort, (effort_to_adjacent, ar, ac))
+                return effort
+            for x, y in get_adjacents(r, c):
+                resulting_effort = max(effort, abs(heights[x][y] - heights[r][c]))
+                if resulting_effort < min_efforts[x][y]:
+                    min_efforts[x][y] = resulting_effort
+                    heapq.heappush(pq, (resulting_effort, (x, y)))
         
-                    
-        
-        
+        return None
