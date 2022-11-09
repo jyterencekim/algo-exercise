a61c16fdd14e3dac50bfaa39e40986060f80225a
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2022-11-09 22:42:10 +0900

    Time: 66 ms (22.80%), Space: 14 MB (13.54%) - LeetHub

---
+++ 1317-convert-integer-to-the-sum-of-two-no-zero-integers/1317-convert-integer-to-the-sum-of-two-no-zero-integers.py
@@ -0,0 +1,16 @@
+class Solution:
+    def getNoZeroIntegers(self, n: int) -> List[int]:
+        def is_nonzero(x: int) -> bool:
+            if x == 0:
+                return False
+            while x:
+                if x % 10 == 0:
+                    return False
+                x = x // 10
+            return True
+        
+        for i in range(1, n):
+            complement = n - i
+            if is_nonzero(complement) and is_nonzero(i):
+                return [i, complement]
+            
