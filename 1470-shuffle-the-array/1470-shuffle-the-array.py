7843d7ff8fe7b0f4bfaac1552c7750a78587e4d8
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2022-11-21 18:49:19 +0900

    Time: 130 ms (47.18%), Space: 14.1 MB (41.25%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,8 +1,6 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        result = []
         half = len(nums) // 2
         for ptr in range(half):
             yield nums[ptr]
             yield nums[ptr + half]
-        return result
