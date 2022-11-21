f5dca2e3f8509adf27c3dbc122f97d560db82b2f
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2022-11-21 18:49:06 +0900

    Time: 136 ms (37.76%), Space: 14 MB (99.58%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -3,6 +3,6 @@ class Solution:
         result = []
         half = len(nums) // 2
         for ptr in range(half):
-            result.append(nums[ptr])
-            result.append(nums[ptr + half])
+            yield nums[ptr]
+            yield nums[ptr + half]
         return result
