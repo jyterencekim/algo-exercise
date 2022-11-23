97a284110f375c24082d7fe53c5ce2377abc348b
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2022-11-23 20:11:36 +0900

    Time: 903 ms (55.55%), Space: 37.3 MB (41.20%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.py
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.py
@@ -10,39 +10,6 @@ class Solution:
         for i in range(n):
             for j in range(i, n):
                 ssums.append(psum[j] - (psum[i - 1] if i > 0 else 0))
-        
-        # returns the pivot index
-        def partition(l: int, r: int) -> int:
-            nonlocal ssums
-            pivot = (l + r) // 2
-            pivot_val = ssums[pivot]
-            ssums[r], ssums[pivot] = ssums[pivot], ssums[r]
-            
-            ptr = frontier = l
-            while ptr < r:
-                if ssums[ptr] < pivot_val:
-                    ssums[frontier], ssums[ptr] = ssums[ptr], ssums[frontier]
-                    frontier += 1
-                ptr += 1
-            ssums[r], ssums[frontier] = ssums[frontier], ssums[r]
-            return frontier
-        
-        def quickselect(l: int, r: int, target: int) -> int:
-            pivot = partition(l, r)
-            if pivot == target:
-                return pivot
-            elif pivot < target:
-                return quickselect(pivot + 1, r, target)
-            return quickselect(l, pivot - 1, target)
                 
-        
-        SS = len(ssums)
-        r_pivot = quickselect(0, SS - 1, right - 1)
-        r_sum = sum(ssums[:r_pivot + 1])
-        
-        if left > 1:
-            l_pivot = quickselect(0, r_pivot, left - 1) 
-            l_sum = sum(ssums[:l_pivot])
-        else:
-            l_sum = 0
-        return (r_sum - l_sum) % MOD
+        ssums.sort()
+        return sum(ssums[left - 1:right]) % MOD
