ff5ccaf2f6c999e4b6037103b902b216899882ee
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2023-01-08 18:30:30 +0900

    Time: 522 ms (89.51%), Space: 20.2 MB (38.31%) - LeetHub

---
+++ 1730-shortest-path-to-get-food/1730-shortest-path-to-get-food.py
@@ -0,0 +1,43 @@
+class Solution:
+    def getFood(self, grid: List[List[str]]) -> int:
+        START, FOOD, FREE, OBSTACLE = '*', '#', 'O', 'X'
+        
+        start = None
+        
+        ROWS, COLS = len(grid), len(grid[0])
+        
+        for row in range(ROWS):
+            for col in range(COLS):
+                if grid[row][col] == START:
+                    start = (row, col)
+                    break        
+        
+        q = deque([(start, 0)]) # (row, col), dist
+        visited = set()
+        
+        def get_adjacents(pos: Tuple[int]) -> Tuple[int]:
+            r, c = pos
+            if r + 1 < ROWS:
+                yield r + 1, c
+            if c + 1 < COLS:
+                yield r, c + 1
+            if r > 0:
+                yield r - 1, c
+            if c > 0:
+                yield r, c - 1
+            
+        while q:
+            pos, d = q.popleft()
+            r, c = pos
+            if grid[r][c] == FOOD:
+                return d
+            
+            visited.add(pos)
+            
+            for next_pos in get_adjacents(pos):
+                x, y = next_pos
+                if next_pos not in visited and grid[x][y] != OBSTACLE:
+                    visited.add(next_pos)
+                    q.append((next_pos, d + 1))
+        
+        return -1
