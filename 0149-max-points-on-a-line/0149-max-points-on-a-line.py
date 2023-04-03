aa24339f4b42c0eb97a44ca78d374fd0d4c9a787
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2023-04-03 19:55:23 +0900

    Time: 410 ms (11.38%), Space: 50.3 MB (5.13%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -3,34 +3,17 @@ from decimal import *
 
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
-        # lines can be defined by their x,y-intercepts and their slope
-        def get_y_intercept(point: Tuple[int], slope: float) -> float:
-            x, y = point
-            
-            if slope == math.inf:
-                if x == 0:
-                    return 0
-                return None
-            
-            return y - (slope * x)
-        
-        def get_x_intercept(point: Tuple[int], slope: float, y_intercept: float) -> float:
-            x, _ = point
-            if not slope or slope == math.inf or not y_intercept:
-                return x
-                
-            return -y_intercept / slope
-        
-        def get_slope(a: Tuple[int], b: Tuple[int]) -> float:
-            ax, ay = a
-            bx, by = b
-            diff_x = bx - ax
-            diff_y = by - ay
-            
-            if diff_x == 0:
-                return math.inf
-            
-            return Decimal(diff_y) / Decimal(diff_x)
+        def to_line(a: Tuple[int], b: Tuple[int]) -> Tuple[float]:
+            # y = slope * x + y_intercept
+            # returns (slope, x-intercept, y-intercept)
+            x1, y1 = a
+            x2, y2 = b
+            if x1 == x2:
+                return math.inf, None, x1
+            slope = Decimal(y2 - y1) / Decimal(x2 - x1)
+            y_intercept = y1 - (slope * x1)
+            x_intercept = -y_intercept / slope if slope else (0 if y1 == 0 else None)
+            return slope, x_intercept, y_intercept
         
         points_by_lines = defaultdict(set) # (slope, b) -> set of pts
         
@@ -42,11 +25,9 @@ class Solution:
         for i in range(N - 1):
             for j in range(i + 1, N):
                 a, b = tuple(points[i]), tuple(points[j])
-                slope = get_slope(a, b)
-                y_intercept = get_y_intercept(a, slope)
-                x_intercept = get_x_intercept(a, slope, y_intercept)
-                points_by_lines[(slope, x_intercept, y_intercept)].add(a)
-                points_by_lines[(slope, x_intercept, y_intercept)].add(b)
+                line = to_line(a, b)
+                points_by_lines[line].add(a)
+                points_by_lines[line].add(b)
         
         return max(len(points) for points in points_by_lines.values())
             
