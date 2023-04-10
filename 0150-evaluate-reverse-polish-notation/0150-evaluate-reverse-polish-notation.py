7c8eac6cf017f0702d3d1ef935e5a0373fb51f8e
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2023-04-10 19:54:15 +0900

    Time: 79 ms (14.35%), Space: 14.3 MB (92.39%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,21 +1,13 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-        ADD, SUBTRACT, MULTIPLY, DIVIDE = "+-*/"
+        OPERATORS = "+-*/"
+        ADD, SUBTRACT, MULTIPLY, DIVIDE = OPERATORS
         
-        def is_number(x: str) -> bool:
-            # assuming one char operators only
-            return x.isnumeric() or (len(x) > 1 and x[1:].isnumeric())
-        
-        def to_number(x: str) -> int:
-            # assuming ints of valid forms only
-            if not x[0].isnumeric():
-                return -1 * int(x[1:])
-            return int(x)
         
         for token in tokens:
-            if is_number(token):
-                stack.append(to_number(token))
+            if token not in OPERATORS:
+                stack.append(int(token))
             else:
                 # assuming only correct notations
                 b, a = stack.pop(), stack.pop()
@@ -27,8 +19,7 @@ class Solution:
                 elif token == MULTIPLY:
                     result = a * b
                 else:
-                    is_negative = a * b < 0
-                    result = a // b if not is_negative else -(abs(a) // abs(b))
+                    result = int(a / b)
                 stack.append(result)
         
         return stack[-1]
