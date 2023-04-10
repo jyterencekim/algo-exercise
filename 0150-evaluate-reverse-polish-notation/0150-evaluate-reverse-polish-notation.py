9c0203b57daf69c185fb5466980ef848281d04cf
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2023-04-10 19:54:36 +0900

    Time: 62 ms (89.91%), Space: 14.3 MB (45.44%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -4,9 +4,13 @@ class Solution:
         OPERATORS = "+-*/"
         ADD, SUBTRACT, MULTIPLY, DIVIDE = OPERATORS
         
+        def is_number(x: str) -> bool:
+            # assuming one char operators only
+            return x.isnumeric() or (len(x) > 1 and x[1:].isnumeric())
+        
         
         for token in tokens:
-            if token not in OPERATORS:
+            if is_number(token):
                 stack.append(int(token))
             else:
                 # assuming only correct notations
