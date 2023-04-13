d47ea21c734af55912f86512d14eb3c74924cdd8
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2023-04-13 21:32:10 +0900

    Time: 1269 ms (11.77%), Space: 13.9 MB (85.33%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.py
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.py
@@ -1,40 +1,31 @@
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
-        def satisfies(counter: Counter) -> bool:
-            return all(count >= k for count in counter.values())
+        N = len(s)
+        unique_chars = set(s)
+        max_length = 0
         
-        def solve(s: str) -> int:
-            counts = []
-            for c in s:
-                last = counts[-1] if counts else Counter()
-                counts.append(last + Counter({c: 1}))
-
-            longest = 0
-            for end in range(len(s)):
-                for start in range(end + 1):
-                    count = counts[end] - counts[start - 1] if start > 0 else counts[end]
-                    if satisfies(count):
-                        length = end - start + 1
-                        longest = max(longest, length)
-                        break
-
-            return longest
-        
-        s = list(s)
-        frequencies = Counter(s)
-        candidates = set()
-        for c, f in frequencies.items():
-            if f >= k:
-                candidates.add(c)
+        for u in range(1, len(unique_chars) + 1):
+            start, end = 0, 0
+            unique_carry = Counter()
+            satisfied_uniques = 0
+            
+            while end < N:
+                unique_carry[s[end]] += 1
+                if unique_carry[s[end]] == k:
+                    satisfied_uniques += 1
+                    
+                while len(+unique_carry) > u:
+                    if unique_carry[s[start]] == k:
+                        satisfied_uniques -= 1
+                    unique_carry[s[start]] -= 1
+                    start += 1
                 
-        for i, c in enumerate(s):
-            if c not in candidates:
-                s[i] = ' '
+                if satisfied_uniques == len(+unique_carry):
+                    length = end - start + 1
+                    max_length = max(max_length, length)
+                    
+                end += 1
         
-        s = ''.join(s)
-        targets = s.split()
-        
-        return max(solve(x) for x in targets) if targets else 0
-        
+        return max_length
         
                 
