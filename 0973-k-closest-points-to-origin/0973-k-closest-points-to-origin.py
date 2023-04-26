732afe2581e99a26c331a20e8f3a997a141db4a1
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2023-04-26 10:25:08 +0900

    Time: 1231 ms (5.00%), Space: 20.2 MB (77.30%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -6,24 +6,28 @@ class Solution:
             return math.sqrt(x ** 2 + y ** 2)
         
         def get_kth(lo: int, hi: int, relative_k_zero: int) -> Tuple[int]:
-            # inclusive lo, hi
-            random_index = random.randint(lo, hi)
-            pivot = points[random_index]
-            points[hi], points[random_index] = points[random_index], points[hi]
-            # pivot at hi
-            frontier = lo
-            for pt in range(lo, hi):
-                if get_distance(points[pt]) <= get_distance(pivot):
-                    points[pt], points[frontier] = points[frontier], points[pt]
-                    frontier += 1
-            
-            points[frontier], points[hi] = points[hi], points[frontier]
-            relative_pos = frontier - lo
-            if relative_pos == relative_k_zero:
-                return points[:frontier + 1]
-            elif relative_pos < relative_k_zero:
-                return get_kth(frontier + 1, hi, relative_k_zero - relative_pos - 1)
-            return get_kth(lo, frontier - 1, relative_k_zero)
+            while lo <= hi:
+                # inclusive lo, hi
+                random_index = random.randint(lo, hi)
+                pivot = points[random_index]
+                points[hi], points[random_index] = points[random_index], points[hi]
+                # pivot at hi
+                frontier = lo
+                for pt in range(lo, hi):
+                    if get_distance(points[pt]) <= get_distance(pivot):
+                        points[pt], points[frontier] = points[frontier], points[pt]
+                        frontier += 1
+
+                points[frontier], points[hi] = points[hi], points[frontier]
+                relative_pos = frontier - lo
+                if relative_pos == relative_k_zero:
+                    return points[:frontier + 1]
+                elif relative_pos < relative_k_zero:
+                    lo = frontier + 1
+                    relative_k_zero -= (relative_pos + 1)
+                else:
+                    hi = frontier - 1
+                
         
         return get_kth(0, len(points) - 1, k - 1)
                 
