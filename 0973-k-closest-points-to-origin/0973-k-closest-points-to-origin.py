e9b7303c46736beadf4e1e55d9b515ccac7e45fe
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2023-04-26 10:26:26 +0900

    Time: 889 ms (15.03%), Space: 23.6 MB (5.55%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,6 +1,8 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        points = [tuple(x) for x in points]
         
+        @cache
         def get_distance(point: Tuple[int]) -> int:
             x, y = point
             return math.sqrt(x ** 2 + y ** 2)
