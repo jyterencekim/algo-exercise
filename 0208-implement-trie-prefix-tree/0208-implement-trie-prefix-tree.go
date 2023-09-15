416221048c55dd8ddafa055cbd5a809f81e97c9d
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2023-09-15 20:11:31 +0900

    Time: 57 ms (54.21%), Space: 15.8 MB (15.14%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.go
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.go
@@ -1,51 +1,58 @@
-type Trie struct {
-    isWord bool
-    nexts map[byte]*Trie
+type Node struct {
+    end bool
+    nexts map[rune]*Node
 }
 
+type Trie struct {
+    root *Node
+}
+
+func NewNode() *Node {
+    return &Node {end: false, nexts: make(map[rune]*Node, 0)}
+}
 
 func Constructor() Trie {
-    return Trie {isWord: false, nexts: make(map[byte]*Trie, 0)}
+    root := NewNode()
+    return Trie {root: root}
 }
 
 
 func (this *Trie) Insert(word string)  {
-    if len(word) == 0 {
-        this.isWord = true
-        return
+    node := this.root
+    
+    for _, c := range(word) {
+        if node.nexts[c] == nil {
+            node.nexts[c] = NewNode()
+        }
+        node = node.nexts[c]
     }
-    c := word[0]
-    _, ok := this.nexts[c]
-    if !ok {
-        this.nexts[c] = &Trie {isWord: false, nexts: make(map[byte]*Trie, 0)}
-    }
-    this.nexts[c].Insert(word[1:])
+    node.end = true
 }
 
 
 func (this *Trie) Search(word string) bool {
-    if len(word) == 0 {
-        return this != nil && this.isWord
+    node := this.root
+    
+    for _, c := range(word) {
+        if node.nexts[c] == nil {
+            return false
+        }
+        node = node.nexts[c]
     }
-    c := word[0]
-    next, ok := this.nexts[c]
-    if !ok {
-        return false
-    }
-    return next.Search(word[1:])
+    return node.end
 }
 
 
 func (this *Trie) StartsWith(prefix string) bool {
-    if len(prefix) == 0 {
-        return this != nil
+    node := this.root
+    
+    for _, c := range(prefix) {
+        if node.nexts[c] == nil {
+            return false
+        }
+        node = node.nexts[c]
     }
-    c := prefix[0]
-    next, ok := this.nexts[c]
-    if !ok {
-        return false
-    }
-    return next.StartsWith(prefix[1:])
+    return node != nil
 }
 
 
