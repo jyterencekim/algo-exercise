12d9bc845e52358de352606335ec2c9085d24fa9
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2024-05-01 20:21:32 +0900

    Time: 377 ms (20.51%) | Memory: 44.7 MB (18.51%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.py
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.py
@@ -5,13 +5,42 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        def to_list(head: Optional[ListNode]) -> List[int]:
-            result = []
-            while head:
-                result.append(head.val)
-                head = head.next
-            return result
+        def get_halfpoint(head: Optional[ListNode]) -> Optional[ListNode]:
+            """
+            returns the node right before the second half
+            """
+            slow, quick = head, head
+            proceed_slow = False
+            while quick.next:
+                quick = quick.next
+                if proceed_slow:
+                    slow = slow.next
+                proceed_slow = not proceed_slow
+            return slow 
+
+        def reverse(head: Optional[ListNode], prev: Optional[ListNode]) -> Optional[ListNode]:
+            if not head.next:
+                head.next = prev
+                return head
+            next_node = head.next
+            head.next = prev
+            return reverse(next_node, head)
         
-        lst = to_list(head)
-        N = len(lst)
-        return max([x + y for x, y in zip(lst[:N//2], lst[N-1:N//2-1:-1])])
+        # separate the two parts
+        halfpoint = get_halfpoint(head)
+        x = head
+        y = halfpoint.next
+        halfpoint.next = None
+
+        y = reverse(y, None)
+
+        max_pair_sum = 0
+        while x and y:
+            pair_sum = x.val + y.val
+            max_pair_sum = max(max_pair_sum, pair_sum)
+            x = x.next
+            y = y.next
+        
+        return max_pair_sum
+
+
