cec7307cbacffe1f0cd0ded04346fc2cb6c42f44
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2024-05-02 20:03:42 +0900

    Time: 107 ms (98.86%) | Memory: 31.5 MB (27.27%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -7,15 +7,16 @@
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         count = 0
-        def traverse(node: TreeNode, max_so_far=-math.inf) -> None:
-            if not node:
-                return
+        stack = [] # (node, max_so_far)
+        stack.append((root, -math.inf))
+        while stack:
+            node, max_so_far = stack.pop()
             if node.val >= max_so_far:
-                nonlocal count
                 count += 1
                 max_so_far = node.val
-            traverse(node.left, max_so_far)
-            traverse(node.right, max_so_far)
+            if node.left:
+                stack.append((node.left, max_so_far))
+            if node.right:
+                stack.append((node.right, max_so_far))
         
-        traverse(root)
         return count
