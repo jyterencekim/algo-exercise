101c73d8ac60658825fc946add48f38af1dfbfa0
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2024-05-06 21:03:41 +0900

    Time: 361 ms (76.55%) | Memory: 38.4 MB (57.64%) - LeetSync

---
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -0,0 +1,37 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.left_max_heap = [] # negative
+        self.right_min_heap = []
+
+    def addNum(self, num: int) -> None:
+        if (not self.left_max_heap and not self.right_min_heap):
+            heapq.heappush(self.right_min_heap, num)
+            return
+        if self.right_min_heap[0] <= num:
+            heapq.heappush(self.right_min_heap, num)
+            if self.right_min_heap and len(self.right_min_heap) > len(self.left_max_heap) + 1:
+                popped = heapq.heappop(self.right_min_heap)
+                heapq.heappush(self.left_max_heap, -popped)
+        else:
+            heapq.heappush(self.left_max_heap, -num)
+            if self.left_max_heap and len(self.left_max_heap) > len(self.right_min_heap):
+                popped = heapq.heappop(self.left_max_heap)
+                heapq.heappush(self.right_min_heap, -popped)
+
+        
+
+    def findMedian(self) -> float:
+        if (len(self.left_max_heap) + len(self.right_min_heap)) % 2 == 0:
+            left_max = -self.left_max_heap[0] if self.left_max_heap else 0
+            right_min = self.right_min_heap[0] if self.right_min_heap else 0
+            
+            return (float(left_max) + float(right_min)) / 2.0
+        return self.right_min_heap[0] if self.right_min_heap else None
+        
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
