3647c20d384dcfbf6c3b6722f370cfe338987b75
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2024-07-29 18:54:32 +0900

    Time: 832 ms (5.93%) | Memory: 24.4 MB (22.94%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -11,17 +11,14 @@ class Solution:
         left, right = 0, 0
         N = len(fruits)
         counter = Counter()
-        active = set()
         longest = 1
 
         for right, fruit in enumerate(fruits):
             counter[fruit] += 1
-            if counter[fruit] == 1:
-                active.add(fruit)
-            while len(active) > 2:
+            while len(counter.keys()) > 2:
                 counter[fruits[left]] -= 1
                 if counter[fruits[left]] == 0:
-                    active.remove(fruits[left])
+                    del counter[fruits[left]]
                 left += 1
 
             longest = max(longest, right - left + 1)
