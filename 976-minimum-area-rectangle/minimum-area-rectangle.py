415c40173a210f8b29bbc296639bb0519d3e8fbf
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2024-08-06 22:16:54 +0900

    Time: 1833 ms (10.57%) | Memory: 16.9 MB (26.25%) - LeetSync

--- 976-minimum-area-rectangle/minimum-area-rectangle.py
+++ 976-minimum-area-rectangle/minimum-area-rectangle.py
@@ -11,12 +11,10 @@ class Solution:
         x = 1, 3, 4
         """
 
-        points.sort(key=lambda p:(p[0], p[1]))
         ys_by_x = defaultdict(list)
         for x, y in points:
             ys_by_x[x].append(y)
 
-        points.sort(key=lambda p:(p[1], p[0]))
         xs_by_y = defaultdict(list)
         for x, y in points:
             xs_by_y[y].append(x)
@@ -28,7 +26,7 @@ class Solution:
             for i in range(len(xs) - 1):
                 for j in range(i + 1, len(xs)):
                     xi, xj = xs[i], xs[j]
-                    w = xj - xi
+                    w = abs(xj - xi)
                     for other_y, other_xs in xs_by_y.items():
                         if other_y == y:
                             continue
