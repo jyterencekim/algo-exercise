69846052f0f57d0d32e4a5e25d27b1f54e71d2da
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2024-08-07 20:58:35 +0900

    Time: 221 ms (56.50%) | Memory: 17.9 MB (28.16%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.py
+++ 85-maximal-rectangle/maximal-rectangle.py
@@ -1,7 +1,7 @@
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
         R, C = len(matrix), len(matrix[0])
-        lens = [[0 for _ in range(C)] for _ in range(R)]
+        lens = [[0 for _ in range(C)] for _ in range(R + 1)]
 
         for r in range(R):
             for c in range(C - 1, -1, -1):
@@ -11,17 +11,14 @@ class Solution:
 
         max_area = 0
 
-        for r in range(R):
-            for c in range(C):
-                if lens[r][c]:
-                    running_width = lens[r][c]
-                    max_area = max(max_area, running_width)            
-                    for nr in range(r + 1, R):
-                        running_width = min(running_width, lens[nr][c])
-                        if not running_width:
-                            break
-                        height = nr - r + 1
-                        running_area = height * running_width
-                        max_area = max(max_area, running_area)
+        for c in range(C):
+            stack = []
+            for r in range(R + 1):
+                top = r
+                while stack and stack[-1][0] > lens[r][c]:
+                    prev_w, top = stack.pop()
+                    area = prev_w * (r - top)
+                    max_area = max(max_area, area)
+                stack.append((lens[r][c], top))
         
         return max_area
