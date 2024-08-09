89eaec216568b65a086d241ad00fb3420f8c0e22
Jeong-yeop (Terence) Kim <jyterencekim@gmail.com>
2024-08-09 21:21:11 +0900

    Time: 45 ms (95.83%) | Memory: 17.1 MB (48.77%) - LeetSync

--- 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.py
+++ 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.py
@@ -6,51 +6,29 @@
 #         self.right = right
 class Solution:
     def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
-        nodes = dict() # val -> TreeNode
-        parents = defaultdict(set) # TreeNode -> Set[TreeNode]
+        roots = list()
         to_delete = set(to_delete)
-        visited = set()
 
-        def prepare(node: Optional[TreeNode], parent=None) -> None:
-            if not node:
-                return
-            nodes[node.val] = node
-            parents[node] = parent
-            prepare(node.left, node)
-            prepare(node.right, node)
-        
-        def bfs(node: Optional[TreeNode]) -> None:
-            q = deque([node])
-            while q:
-                n = q.popleft()
-                visited.add(n)
-                for x in (n.left, n.right, parents[n]):
-                    if x and x not in visited:
-                        visited.add(x)
-                        q.append(x)
-        
-        def delete(node_val: int) -> None:
-            node = nodes[node_val]
-            p = parents[node]
-            if p:
-                if p.left == node:
-                    p.left = None
-                else:
-                    p.right = None
+        q = deque([root])
+
+        while q:
+            node = q.popleft()
             if node.left:
-                parents[node.left] = None
+                q.append(node.left)
+                if node.left.val in to_delete:
+                    node.left = None
             if node.right:
-                parents[node.right] = None
-            del nodes[node_val]
-
-        prepare(root)
-        for node_val in to_delete:
-            delete(node_val)
-
-        roots = []
-        for v, node in nodes.items():
-            if node not in visited:
-                roots.append(node)
-                bfs(node)
+                q.append(node.right)
+                if node.right.val in to_delete:
+                    node.right = None
+            if node.val in to_delete:
+                if node.left:
+                    roots.append(node.left)
+                if node.right:
+                    roots.append(node.right)
         
+        if root.val not in to_delete:
+            roots.append(root)
+
         return roots
+
